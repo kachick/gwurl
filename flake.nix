0b29fcce52aff965233c2e8a437fa7fe70f22c34
Kenichi Kamiya <kachick1@gmail.com>
2024-05-25 00:53:17 +0900

    Bump to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -5,16 +5,14 @@
     #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, edge-nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
         updaterVersion =
           if (self ? shortRev)
           then self.shortRev
@@ -28,17 +26,17 @@
               # https://github.com/kachick/dotfiles/pull/228
               bashInteractive
 
-              edge-pkgs.nil
-              edge-pkgs.nixpkgs-fmt
-              edge-pkgs.dprint
-              edge-pkgs.yamlfmt
-              edge-pkgs.typos
-              edge-pkgs.go-task
-              edge-pkgs.go_1_22
+              nil
+              nixpkgs-fmt
+              dprint
+              yamlfmt
+              typos
+              go-task
+              go_1_22
             ];
           };
 
-        packages.gwurl = edge-pkgs.buildGo122Module {
+        packages.gwurl = pkgs.buildGo122Module {
           pname = "gwurl";
           version = updaterVersion;
           src = pkgs.lib.cleanSource self;
